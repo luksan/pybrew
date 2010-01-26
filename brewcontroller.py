592a4b44598fa14e28849c7a89874a79993f3020
Lukas Sandström <luksan@gmail.com>
2010-01-26 12:41:05 +0100

    Use PyQt4 instead of wxWidgets.

--- brewcontroller.py
+++ brewcontroller.py
@@ -13,22 +13,13 @@ SERIAL_STATE_ERROR = 3
 # GR läser regulatorns börvärde
 # SR <temperatur> sätter regulatorns börvärde
 
-class BrewController():
-    def __init__(self):
-        self.sport = None
-        
-        self.serial_state = SERIAL_STATE_INIT
-        self.ticker = 0
-        
-        self.read_temp1 = -255
-        self.has_read_temp1 = False
-        
-        self.set_temp1 = 0
-        self.do_set_temp1 = False
-        
-        self.send_queue = []
+class BrewControllerException(Exception):
+    pass
 
-        self.last_command_ok = False
+class BrewController():
+    def __init__(self, port_name = None):
+        self.sport = None
+        self.serial_state = SERIAL_STATE_INIT
         
         self.VALVES = {
             '0': 'Ventil 1',
@@ -36,91 +27,83 @@ class BrewController():
             '2': 'Ventil 3',
             '3': 'Ventil 4',
             }
-
+        
+        if port_name != None:
+            self.open_port(port_name)
+        
+    def open_port(self, port_name):
         try:
-            self.sport = serial.Serial('/dev/ttyUSB0')
-            self.sport.timeout = 0.1
+            self.sport = serial.Serial(port_name)
+            self.sport.timeout = 0.2
         except Exception, e:
             print e
-            raise Exception('Failed to init COM4')
+            #raise BrewControllerException('Failed to init COM4')
             class A:
+                def __init__(self):
+                    self.s = "Fake controller ready\n"
                 def readlines(self):
-                    return []
+                    return [self.read()]
+                def read(self):
+                    s = self.s
+                    self.s = "OK\n"
+                    if s == "GT 0\n":
+                        self.s = "32\n"
+                    if s.startswith("GV"):
+                        self.s = "0\n"
+                    return s
+                def readline(self):
+                    return self.read()
+                def write(self, s):
+                    self.s = s
             self.sport = A()
-            return
-
-    def _parse_serial(self, lines):
-        self.last_command_ok = False
         
-        if (len(lines) < 1):
-            return
-        if (lines[0].find("accepting commands") != -1):
-            self.serial_state = SERIAL_STATE_ACTIVE
-            print 'Serial line active'
-            return
-        if (self.serial_state != SERIAL_STATE_ACTIVE):
-            return
+        # Flush initialization lines
+        for l in self.sport.readlines():
+            print l
 
-        for i in range(0, len(lines)):
-            if (lines[0].find('GT') != -1):
-                try:
-                    self.read_temp1 = int(lines[1])
-                    self.has_read_temp1 = True
-                    self.last_command_ok = True
-                except:
-                    self.read_temp1 = -255
-                    self.has_read_temp1 = False
-            if (lines[0].find('SR') != -1):
-                try:
-                    result = lines[1]
-                    if (result.find('OK') != -1):
-                        self.last_command_ok = True
-                except:
-                    pass
-
-    def send(self, line):
-        self.send_queue.append(line)
+    def __send(self, line):
+        if line[-1] != '\n':
+            line = line + '\n'
+        self.sport.write(line)
+        r = self.sport.readline()
+        if r != line:
+            raise BrewControllerException("Read error. Sent '"+line + "', received '" + r + "'")
+        result = self.sport.readline().strip()
+        if result in ('NCK', 'NOK'):
+            return False
+        if result == 'OK':
+            return True
+        return result
 
     def set_valve_open(self, valve, open):
-        print "Setting", self.VALVES[valve], open
+        """Set the valve to open or closed."""
         s = '0'
         if open:
             s = '1'
-        self.send("SV " + str(valve) + " " + s)
-
-    def set_temp(this, temp):
-        self.send("SR " + str(this.set_temp1))
-        
-    def get_temp(this):
-        return this.read_temp1
-
-    def isready(self):
-        if ((self.serial_state == SERIAL_STATE_ACTIVE) and
-            (self.has_read_temp1)):
-            return True
-        else:
-            return False
-
-    def run(self):      
-        lines = self.sport.readlines()
-        self._parse_serial(lines)
-        self.ticker += 1
-
-        if (self.serial_state != SERIAL_STATE_ACTIVE):
-            return
-
-        if ((self.ticker % 2) == 0):
-            self.sport.write("GT 0")
-
-        if (((self.ticker+1) % 2) == 0):
-            for l in self.send_queue:
-                self.sport.write(l)
-            self.send_queue = []
-
-def main():
-    bc = BrewController()
-    while(True):
-        bc.run()
-        time.sleep(0.1)
+        r = self.__send("SV " + str(valve) + " " + s)
+        if r != True:
+            raise BrewControllerException("Valve command failed. Got: " + str(r))
     
-#main()
+    def get_valve_open(self, valve):
+        r = self.__send("GV " + str(valve))
+        if r == "1":
+            return True
+        if r == "0":
+            return False
+        raise BrewControllerException("GV command returned bad response: '" + r + "'.")
+
+    def set_temp(self, temp):
+        """Sets target temperature to temp and returns the actual
+           target temperature from the controller."""
+        if not self.__send("SR " + str(temp)):
+            raise BrewControllerException("Setting of target temp failed.")
+        return self.get_target_temp()
+    
+    def get_target_temp(self):
+        """Read the target temperature from the controller."""
+        r = self.__send("GR")
+        return int(r)
+
+    def get_temp(self, sensor_id):
+        """Read the actual temperature from sensor sensor_id."""
+        return self.__send("GT " + str(sensor_id))
---
+++ pybrew.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import serial
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.Qwt5 import *
+
+from pybrewMainWindow import MainWindow
+
+from brewcontroller import BrewController
+
+class Pybrew(MainWindow):
+    def __init__(self):
+        MainWindow.__init__(self)
+
+        self.tempUpdateInterval = 1000 # update interval in milliseconds
+
+        try:
+            self.bc = BrewController('/dev/ttyUSB0')
+        except Exception as e:
+            QMessageBox.critical(None, "Fatal error", str(e))
+            sys.exit(1)
+
+        buttons = self.bc.VALVES.keys()
+        buttons.sort()
+        self.valve_buttons = []
+        def get_callback(id, button):
+            def cb():
+                self.valve_button_clicked(id, button)
+            return cb
+
+        for k in buttons:
+            v = QPushButton(self.bc.VALVES[k], self)
+            v.connect(v, SIGNAL('clicked()'), get_callback(k, v))
+            v.setObjectName(k)
+            v.setCheckable(True)
+            self.valveButtonLayout.addWidget(v)
+            self.valve_buttons.append(v)
+            
+        self.tempCurve = QwtPlotCurve("Temperature")
+        self.tempCurve.attach(self.tempQwtPlot)
+        
+        self.tempXData = []
+        self.tempYData = []
+        self.tempCurve.setData(range(100), range(100))
+        
+        self.tempUpdateTimer = QTimer(self)
+        self.connect(self.tempUpdateTimer, SIGNAL('timeout()'), self.tempUpdateEvent)
+        self.tempUpdateTimer.start(self.tempUpdateInterval)
+            
+    def setTargetTempEvent(self, event):
+        temp = self.targetTempLineEdit.text()
+        print "set temp to", temp
+        self.bc.set_temp(temp)
+    
+    def tempUpdateEvent(self):
+        temp = self.bc.get_temp("0")
+        if not self.tempXData:
+            self.tempXData = [0]
+        else:
+            self.tempXData.append(self.tempXData[-1] + self.tempUpdateInterval/1000.0)
+        self.tempYData.append(int(temp))
+        self.tempCurve.setData(self.tempXData, self.tempYData)
+        self.tempQwtPlot.replot()
+        
+    def valve_button_clicked(self, valve_id, button):
+        self.bc.set_valve_open(valve_id, button.isChecked())
+        is_open = self.bc.get_valve_open(valve_id)
+        button.setChecked(is_open)
+        pal = button.palette()
+        if is_open:
+            color = Qt.green
+        else:
+            color = Qt.yellow
+        pal.setColor(button.backgroundRole(), color)
+        button.setPalette(pal)
+
+    def closeEvent(self, ev):
+        sys.exit(0)
+
+def main():
+    qApp = QApplication(sys.argv)
+    pybrew = Pybrew()
+    pybrew.show()
+    sys.exit(qApp.exec_())
+
+if __name__ == "__main__":
+    main()
+
+app = App(0)
+app.MainLoop()
